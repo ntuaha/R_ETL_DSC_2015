bbe1a861a3657927261d57d499e59a0598f064db
aha <ntuaha@gmail.com>
2015-07-12 14:41:26 +0800

    publishing deck

--- script.R
+++ script.R
@@ -1,13 +1,19 @@
 library(slidify)
-author(deckdir="/Users/aha/Dropbox/Project/2015/03_DSC/2015",open_rmd=FALSE)
+library(dplyr)
+library(ggplot2)
+author(deckdir="/Users/aha/Dropbox/Project/2015/03_DSC/2015",open_rmd=FALSE,use_git=FALSE)
 
+#更新使用
+slidify("index.Rmd");
+publish(user = "ntuaha", repo = "R_ETL_DSC_2015", host = 'github')
 #會有一些bug
 #Error in if (body$content == "") { :
 #  missing value where TRUE/FALSE needed
 #install_version("stringr", version="0.6.2", type = "source")
 
-slidify("index.Rmd")
+#slidify("index.Rmd")
 # replace USER and REPO with your username and reponame
 
-publish(user = "ntuaha", repo = "R_ETL_DSC_2015", host = 'github')
+#publish(user = "ntuaha", repo = "R_ETL_DSC_2015", host = 'github')
+slidify("index.Rmd");publish(user = "ntuaha", repo = "R_ETL_DSC_2015", host = 'github')
 
