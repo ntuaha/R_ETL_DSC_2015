93180b5d79a9c2b0470865137996e494e10c2b21
aha <ntuaha@gmail.com>
2015-07-12 19:47:03 +0800

    publishing deck

--- script.R
+++ script.R
@@ -6,6 +6,10 @@ author(deckdir="/Users/aha/Dropbox/Project/2015/03_DSC/2015",open_rmd=FALSE,use_
 #更新使用
 slidify("index.Rmd");
 publish(user = "ntuaha", repo = "R_ETL_DSC_2015", host = 'github')
+
+#library(swirl)
+#install_course_url("https://github.com/ntuaha/R_ETL_DSC_2015/raw/gh-pages/DSC2015.zip", multi = TRUE)
+#swirl()
 #會有一些bug
 #Error in if (body$content == "") { :
 #  missing value where TRUE/FALSE needed
